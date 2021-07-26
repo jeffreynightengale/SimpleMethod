7902c24d3de6e80125620f619582d57762cc45a4
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-07-26 15:23:50 -0500

    Finished

---
+++ SimpleMethod/Speak/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What animal would you like to hear(dog, monkey, goat)?");
+            string answer = Console.ReadLine().ToLower();
+
+            string sound = Speak(answer);
+            Console.WriteLine($"A {answer} makes a {sound}");
+        }
+
+        static string Speak(string animalNoise)
+        {
+            string sound;
+            
+            if (animalNoise == "dog")
+            {
+                sound = "woof-woof";
+                return sound;
+            }
+            else if (animalNoise == "monkey")
+            {
+                sound = "ooh-oohh ahh-ahhh";
+                return sound;
+            }
+            else if (animalNoise == "goat")
+            {
+                sound = "blehhhhhhhhhhhhhhhhhhhhhhhhhhhhh";
+                return sound;
+            }
+            else
+            {
+                sound = "meow, oink, achoooo";
+                return sound;
+            }
+        }
+    }
+}
